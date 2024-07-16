f12353c1db6ca291a5412a923a422eed05300908
CodeurDHonneur <kkodjoaugustin@gmail.com>
2024-07-16 18:22:13 +0200

    premier commit

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+   
+    return view('layouts.master');
+});
+
+Route::get('/contact-us', function() {
+    return view('layouts.contact');
+});
+
